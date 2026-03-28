6f6ddb519419a344336d15cb98f2ecb92c291c67
pitrilovescattt <fnurazizah600@gmail.com>
2026-03-28 21:25:24 +0700

    Add function to alert successful class join
    Added a new function 'gabung' that alerts a success message.

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-function showSection(sectionId) {
-    document.getElementById('materi').style.display = 'none';
-    document.getElementById('tugas').style.display = 'none';
-
-    document.getElementById(sectionId).style.display = 'block';
-}
-
-function addTask() {
-    let input = document.getElementById("taskInput");
-    let taskText = input.value;
-
-    if (taskText === "") return;
-
-    let li = document.createElement("li");
-    li.textContent = taskText;
-
-    document.getElementById("taskList").appendChild(li);
-
-    input.value = "";
+function gabung() {
+    alert("Yay! Kamu berhasil join kelas 🎉");
 }
